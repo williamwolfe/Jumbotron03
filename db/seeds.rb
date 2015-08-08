c8e264a9fd634694d04a7826e60eb406425bf720
WILLIAMWOLFE <william.wolfe@angelo.edu>
2015-08-08 10:37:43 -0500

    first jumbotron 03 commit

---
+++ db/seeds.rb
@@ -0,0 +1,13 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Blog.create!(title: 'grocery shopping', content: 'pickles, eggs, red onion')
+Blog.create!(title: 'wash the car')
+Blog.create!(title: 'register kids for school', content: 'Register Kira for Ruby Junior High and Caleb for Rails High School')
+Blog.create!(title: 'check engine light', content: 'The check engine light is on in the Tacoma')
+Blog.create!(title: 'dog groomers', content: 'Take Pinky and Redford to the groomers on Wednesday the 23rd')
